f58bbe5b72d6c13274ccaa40677e78daf55658d1
ALPHVMANE <alphxrizen@gmail.com>
2025-12-02 00:31:54 -0500

    Task model - notifications

--- app/src/main/java/model/Task.java
+++ app/src/main/java/model/Task.java
@@ -25,6 +25,7 @@ public abstract class Task implements Serializable {
         this.priority = priority;
         this.updatedAt = updatedAt;
         this.createdAt = createdAt;
+        this.enableNotif = enableNotif;
     }
 
     public Task() {
